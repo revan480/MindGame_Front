8b9f67cc6e89d4ede011827d84115f3ffc148206
Rafig Hajili <hacili.rafiq.03@gmail.com>
2022-12-07 03:53:45 +0400

    confirm password page ui

--- src/pages/Auth/Confirm.jsx
+++ src/pages/Auth/Confirm.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import { useNavigate } from "react-router-dom";
+import Button from "../../components/UI/Button";
 
 const Confirm = () => {
-  return <div>Confirm</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex flex-col items-center gap-7">
+      <h2 className="text-3xl font-bold text-center">Confirmation link has sent to your email!</h2>
+      <Button className="w-fit" onClick={() => navigate("/auth/login")}>
+        Back to login
+      </Button>
+    </div>
+  );
 };
 
 export default Confirm;
