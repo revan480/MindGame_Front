63c78939a28c1167eb02129d0a130dc23e512862
Rafig Hajili <hacili.rafiq03@gmail.com>
2022-12-22 00:14:11 +0400

    cleanup

--- src/pages/Room/Room.jsx
+++ src/pages/Room/Room.jsx
@@ -1,23 +1,7 @@
 import React from "react";
-import LevelCard from "../../components/Cards/LevelCard";
-import NumberCard from "../../components/Cards/NumberCard";
 
 const Room = () => {
-  return (
-    <div className="w-full h-screen flex items-center justify-center flex-wrap">
-      <div className="flex gap-16">
-        <NumberCard number={5} />
-        <NumberCard number={31} />
-        <NumberCard number={69} />
-        <NumberCard number={100} />
-      </div>
-      <div className="flex gap-16">
-        <LevelCard level={3} />
-        <LevelCard level={7} />
-        <LevelCard level={12} />
-      </div>
-    </div>
-  );
+  return <div>Room</div>;
 };
 
 export default Room;
